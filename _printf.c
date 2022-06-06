bb35daf1e5f24d0d32a9e101945154691455d63d
Jacques Asinyo <asinyojacques17@gmail.com>
2022-06-06 09:35:05 +0000

    printf

--- _printf.c
+++ _printf.c
@@ -1,51 +1,30 @@
-#include <stdarg.h>
 #include "main.h"
 
 /**
- *_printf - prints to output according to format
- *@format: character string
+ * _printf - Produces output according to a format
+ * @format: Is a character string. The format string
+ * is composed of zero or more directives
  *
- *Return: number of characters printed
- */
-
+ * Return: The number of characters printed (excluding
+ * the null byte used to end output to strings)
+ **/
 int _printf(const char *format, ...)
 {
-	int i = 0, j = 0;
-	int (*f)(va_list);
+	int size;
 	va_list args;
 
-	va_start(args, format);
-	if (format == NULL || !format[i + 1])
+	if (format == NULL)
 		return (-1);
-	while (format[i])
-	{
-		if (format[i] == '%')
-		{
-			if (format[i + 1])
-			{
-				if (format[i + 1] != 'c' && format[i + 1] != 's'
-				&& format[i + 1] != '%' && format[i + 1] != 'd'
-				&& format[i + 1] != 'i')
-				{
-					j += _putchar(format[i]);
-					j += _putchar(format[i + 1]);
-					i++;
-				}
-				else
-				{
-					f = get_func(&format[i + 1]);
-					j += f(args);
-					i++;
-				}
-			}
-		}
-		else
-		{
-			_putchar(format[i]);
-			j++;
-		}
-		i++;
-	}
+
+	size = _strlen(format);
+	if (size <= 0)
+		return (0);
+
+	va_start(args, format);
+	size = handler(format, args);
+
+	_putchar(-1);
 	va_end(args);
-	return (j);
+
+	return (size);
 }
--- main.h
+++ main.h
@@ -1,30 +1,50 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdarg.h>
-#include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <limits.h>
-#include <string.h>
+#include <stdarg.h>
+
+/* utils.c */
+int _strlen(const char *);
+int print(char *);
+char *itoa(long int, int);
+
+/* printf.c */
+int _printf(const char *, ...);
+
+/* handler.c */
+int handler(const char *, va_list);
+int percent_handler(const char *, va_list, int *);
+
+/* printers */
+int print_string(va_list);
+int print_char(va_list);
+int print_integer(va_list);
+int print_binary(va_list);
+int print_rot(va_list);
+int print_unsigned(va_list);
+int print_octal(va_list);
+int print_hexadecimal_low(va_list);
+int print_hexadecimal_upp(va_list);
+int print_pointer(va_list);
+int print_rev_string(va_list);
+
+/* _putchar.c */
+int _putchar(char);
+int buffer(char);
 
 /**
- * struct func_type - type structure
- * @t: pointer to the argument
- * @f: pointer-function associated with the argument
- */
-typedef struct func_type
+ * struct _format - Typedef struct
+ *
+ * @type: Format
+ * @f: The function associated
+ **/
+typedef struct _format
 {
-	char *t;
+	char type;
 	int (*f)(va_list);
-} func_t;
+} format;
 
-int (*get_func(const char *format))(va_list);
-int _putchar(char c);
-int _printf(const char *format, ...);
-int print_str(va_list args);
-int print_char(va_list args);
-int print_pct(va_list args);
-int print_dec(va_list args);
 
 #endif
